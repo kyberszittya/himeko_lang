d8cc03bbe0ea85c672ed15a19a2de412f852c0ae
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-04-25 01:39:44 +0200

    Finally references are unfolded

--- himeko_lang_core/lang/ast/himeko_ast.py
+++ himeko_lang_core/lang/ast/himeko_ast.py
@@ -1,10 +1,8 @@
 import sys
-import typing
 from typing import List
 from dataclasses import dataclass
 
-from lark import Lark, Transformer, v_args, ast_utils
-from lark.tree import Tree
+from lark import Transformer, v_args, ast_utils
 
 this_module = sys.modules[__name__]
 
@@ -16,14 +14,15 @@ class _Ast(ast_utils.Ast):
 class _Statement(_Ast):
     pass
 
+
 class Value(_Ast, ast_utils.WithMeta):
     pass
 
 
-
 @dataclass
 class ElementName(_Ast):
-    name: str
+    value: str
+
 
 @dataclass
 class HiElementSignature(_Ast):
@@ -33,17 +32,71 @@ class HiElementSignature(_Ast):
 class _HiMetaelement(_Ast):
     name: ElementName
 
+
 class HiMeta(_Ast):
     meta: _HiMetaelement
 
 
+@dataclass
+class _HiAbstractElement(_Ast):
+    signature: HiElementSignature
 
-class _HiElement(_Ast):
-    pass
+    def __init__(self, signature: HiElementSignature):
+        self.signature = signature
+        self._parent = None
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, value):
+        self._parent = value
+
+
+@dataclass
+class ElementReference(_Ast):
+    name: str
+
+    def __init__(self, name):
+        self.name = name.replace('"', '')
+        self._reference = None
+
+    @property
+    def reference(self):
+        return self._reference
+
+    @reference.setter
+    def reference(self, value):
+        self._reference = value
+
+
+@dataclass
+class HiEdgeElement(_Ast):
+    relation_direction: Value
+    reference: ElementReference
+
+    def __init__(self, relation_direction: Value, reference: ElementReference):
+        self.relation_direction = relation_direction
+        self.reference = reference
+
+
+
+@dataclass
+class HiEdge(_HiAbstractElement):
+    vertices: List[HiEdgeElement]
+
+    def __init__(self, signature: HiElementSignature, *vertices: HiEdgeElement):
+        super().__init__(signature)
+        self.vertices = list(vertices)
+
 
 @dataclass
 class HiBody(_Ast):
-    elements: List[_HiElement]
+    root: List[_HiAbstractElement]
+
+    def __init__(self, *root: _HiAbstractElement):
+        self.root = list(root)
 
 
 @dataclass
@@ -51,21 +104,62 @@ class Start(_Ast):
     name: HiMeta
     body: HiBody
 
+
 @dataclass
-class HiNode(_HiElement):
-    signature: HiElementSignature
-    children: typing.Optional[List[_HiElement]]
+class HiNode(_HiAbstractElement):
+    children: List[_HiAbstractElement]
 
-    def __init__(self, signature: HiElementSignature, *children: typing.Optional[List[_HiElement]]):
-        self.signature = signature
-        self.children = children
-
+    def __init__(self, signature: HiElementSignature, *children: _HiAbstractElement):
+        super().__init__(signature)
+        self.children = list(children)
 
 
 class ToAst(Transformer):
     @v_args(inline=True)
     def start(self, x):
         return x
+
+
+def set_parents(node: HiNode):
+    for n in node.children:
+        n.parent = node
+        if isinstance(n, HiNode):
+            set_parents(n)
+        elif isinstance(n, HiEdge):
+            n.parent = node
+
+
+def collect_leafs(node: HiNode):
+    leafs = []
+    for n in node.children:
+        if isinstance(n, HiNode) and len(n.children) > 0:
+            leafs.extend(collect_leafs(n))
+        elif isinstance(n, HiNode) and len(n.children) == 0:
+            leafs.append(n)
+        elif isinstance(n, HiEdge):
+            leafs.append(n)
+    return leafs
+
+
+def unfold_references(edge: HiEdge):
+
+    for v in edge.vertices:
+        context_name = v.reference.name.split('.')[::-1]
+        context = edge
+        for _ in context_name:
+            context = context.parent
+        # Backfold
+        node = None
+        for c in context_name[::-1]:
+            for n in context.children:
+                # Node found in context getting deeper
+                if n.signature.name.value == c:
+                    node = n
+                    break
+            # Update context
+            context = node
+        if node is not None:
+            v.reference.reference = node
 
 
 transformer = ast_utils.create_transformer(this_module, ToAst())
--- himeko_lang_core/lang/simple_examples/himeko_ast_example.py
+++ himeko_lang_core/lang/simple_examples/himeko_ast_example.py
@@ -1,24 +1,31 @@
-from lark import Lark, Transformer, v_args
+from lark import Lark
 
-from lang.ast.himeko_ast import transformer
+from lang.ast.himeko_ast import transformer, set_parents, collect_leafs, HiNode, HiEdge, unfold_references
 
 
 def main(args=None):
-    print(args)
     if args is None:
         p = "../../examples/simple/minimal_example_with_edges.himeko"
     else:
         p = args[0]
-        print("Using path: "+p)
+        print("Using path: " + p)
     parser = None
     with open("../HimekoMetalang.lark") as f:
         parser = Lark(''.join(f.readlines()), parser="lalr")
     with open(p) as f:
         tree = parser.parse(''.join(f.readlines()))
-        print(tree.pretty())
         root = transformer.transform(tree)
-        print(root)
-        print(root.body.elements)
+        set_parents(root.body.root[0])
+        leafs = collect_leafs(root.body.root[0])
+        for leaf in leafs:
+            if isinstance(leaf, HiNode):
+                print("Node: ", leaf.signature.name.value, leaf.parent.signature.name.value)
+            elif isinstance(leaf, HiEdge):
+                print("Edge: ", leaf.signature.name.value, leaf.parent.signature.name.value)
+                unfold_references(leaf)
+                for v in leaf.vertices:
+                    print("Unfolded ref:", v.reference.reference.signature.name.value, v.reference.reference.parent.signature.name.value)
+
 
 
 if __name__ == "__main__":
