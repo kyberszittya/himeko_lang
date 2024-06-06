92f5fdea440d9d4f39357879058cd4294efb0e83
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-06-06 02:05:21 +0200

    Additional tests, shall complete inheritance tomorrow

--- himeko_lang_core/test/test_case_descriptions.py
+++ himeko_lang_core/test/test_case_descriptions.py
@@ -17,10 +17,14 @@ TEST_CASE_FIELDS_WITH_REFERENCE = (
     os.path.join(TEST_CASE_SIMPLE_FOLDER, "minimal_example_fields_with_reference2.himeko"))
 TEST_CASE_FIELDS_WITH_REFERENCE_2 = (
     os.path.join(TEST_CASE_SIMPLE_FOLDER, "minimal_example_fields_with_reference.himeko"))
+TEST_CASE_FIELDS_WITH_HIERARCHY_REF_EDGES = (
+    os.path.join(TEST_CASE_SIMPLE_FOLDER, "minimal_example_with_hierarchy_ref_edges.himeko"))
 TEST_CASE_HIERARCHY_REF_EDGES = (
     os.path.join(TEST_CASE_SIMPLE_FOLDER, "minimal_example_with_hierarchy_ref_edges_evaluation2.himeko"))
 TEST_CASE_HIERARCHY_REF_EDGES_WITH_VALUES = (
     os.path.join(TEST_CASE_SIMPLE_FOLDER, "minimal_example_with_hierarchy_ref_edges_with_values.himeko"))
+TEST_CASE_MULTIPLE_EDGES = (
+    os.path.join(TEST_CASE_SIMPLE_FOLDER, "minimal_example_with_multiple_edges.himeko"))
 
 TEST_CASE_BASIC_FANO = (
     os.path.join(TEST_CASE_BASE_FOLDER, "fano_graph.himeko"))
