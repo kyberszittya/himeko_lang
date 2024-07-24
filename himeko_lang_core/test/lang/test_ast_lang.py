3611429ba546a083b927bd112175e0b226a23112
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-07-24 02:00:15 +0200

    SURG32

--- himeko_lang_core/test/lang/test_ast_lang.py
+++ himeko_lang_core/test/lang/test_ast_lang.py
@@ -9,57 +9,108 @@ class BasicTestLang(unittest.TestCase):
         self.lang_path = os.path.join("..", "..", "lang", "HimekoMetalang.lark")
         self.examples_path = os.path.join("..", "..", "examples")
 
-    def test_ast_lark(self):
+    def load_lang(self):
         with open(self.lang_path) as f:
             lang = f.read()
-            p = Lark(lang, parser='lalr')
+            return Lark(lang, parser="lalr")
+
+    def test_ast_lark(self):
+        p = self.load_lang()
 
     def test_ast_simple_minimal_example(self):
-        p = None
-        with open(self.lang_path) as f:
-            lang = f.read()
-            p = Lark(lang, parser='lalr')
+        p = self.load_lang()
         p0 = os.path.join(self.examples_path, "simple", "minimal_example.himeko")
         with open(p0) as f:
             text = p.parse(f.read())
             print(text)
 
     def test_ast_example_fano_graph(self):
-        p = None
-        with open(self.lang_path) as f:
-            lang = f.read()
-            p = Lark(lang, parser='lalr')
+        p = self.load_lang()
         p0 = os.path.join(self.examples_path, "simple", "base", "fano_graph.himeko")
         with open(p0) as f:
             text = p.parse(f.read())
             print(text)
 
     def test_ast_example_basic_hierarchy(self):
-        p = None
-        with open(self.lang_path) as f:
-            lang = f.read()
-            p = Lark(lang, parser='lalr')
+        p = self.load_lang()
         p0 = os.path.join(self.examples_path, "simple", "minimal_example_basic_hierarchy.himeko")
         with open(p0) as f:
             text = p.parse(f.read())
             print(text)
 
     def test_ast_example_inheritance(self):
-        p = None
-        with open(self.lang_path) as f:
-            lang = f.read()
-            p = Lark(lang, parser='lalr')
+        p = self.load_lang()
         p0 = os.path.join(self.examples_path, "simple", "inheritance_example.himeko")
         with open(p0) as f:
             text = p.parse(f.read())
             print(text)
 
+    def test_ast_example_fields_with_reference(self):
+        p = self.load_lang()
+        p0 = os.path.join(self.examples_path, "simple", "minimal_example_fields_with_reference.himeko")
+        with open(p0) as f:
+            text = p.parse(f.read())
+            print(text)
+
     def test_ast_example_fields_with_reference2(self):
-        p = None
-        with open(self.lang_path) as f:
-            lang = f.read()
-            p = Lark(lang, parser='lalr')
+        p = self.load_lang()
         p0 = os.path.join(self.examples_path, "simple", "minimal_example_fields_with_reference2.himeko")
+        with open(p0) as f:
+            text = p.parse(f.read())
+            print(text)
+
+    def test_ast_example_fields(self):
+        p = self.load_lang()
+        p0 = os.path.join(self.examples_path, "simple", "minimal_example_fields.himeko")
+        with open(p0) as f:
+            text = p.parse(f.read())
+            print(text)
+
+    def test_minimal_example_with_edges(self):
+        p = self.load_lang()
+        p0 = os.path.join(self.examples_path, "simple", "minimal_example_with_edges.himeko")
+        with open(p0) as f:
+            text = p.parse(f.read())
+            print(text)
+
+    def test_minimal_example_with_hierarchy_ref_edges(self):
+        p = self.load_lang()
+        p0 = os.path.join(self.examples_path, "simple", "minimal_example_with_hierarchy_ref_edges.himeko")
+        with open(p0) as f:
+            text = p.parse(f.read())
+            print(text)
+
+    def test_minimal_example_with_hierarchy_ref_edges_evaluation(self):
+        p = self.load_lang()
+        p0 = os.path.join(self.examples_path, "simple", "minimal_example_with_hierarchy_ref_edges_evaluation.himeko")
+        with open(p0) as f:
+            text = p.parse(f.read())
+            print(text)
+
+    def test_minimal_example_with_hierarchy_ref_edges_evaluation2(self):
+        p = self.load_lang()
+        p0 = os.path.join(self.examples_path, "simple", "minimal_example_with_hierarchy_ref_edges_evaluation2.himeko")
+        with open(p0) as f:
+            text = p.parse(f.read())
+            print(text)
+
+    def test_minimal_example_with_hierarchy_ref_edges_evaluation_unknown_references(self):
+        p = self.load_lang()
+        p0 = os.path.join(self.examples_path, "simple", "minimal_example_with_hierarchy_ref_edges_evaluation_unknown_references.himeko")
+        with open(p0) as f:
+            text = p.parse(f.read())
+            print(text)
+
+    def test_minimal_example_with_hierarchy_ref_edges_with_values(self):
+        p = self.load_lang()
+        p0 = os.path.join(self.examples_path, "simple", "minimal_example_with_hierarchy_ref_edges_with_values.himeko")
+        with open(p0) as f:
+            text = p.parse(f.read())
+            print(text)
+
+    def test_minimal_example_with_multiple_edges(self):
+        p = self.load_lang()
+        p0 = os.path.join(self.examples_path, "simple", "minimal_example_with_multiple_edges.himeko")
         with open(p0) as f:
             text = p.parse(f.read())
             print(text)
