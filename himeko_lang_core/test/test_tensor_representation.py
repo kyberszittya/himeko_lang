dfe8c1a4ec53611d7d287f344f1341300bdad519
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-08-21 23:14:20 +0200

    Added star expansion, but will revise it anyway

--- himeko_lang_core/test/test_tensor_representation.py
+++ himeko_lang_core/test/test_tensor_representation.py
@@ -1,5 +1,5 @@
 from himeko.common.clock import NullClock
-from himeko.hbcm.mapping.tensor_mapping import tensor_mapping
+from himeko.hbcm.mapping.tensor_mapping import BijectiveCliqueExpansionTransformation, StarExpansionTransformation
 from lang.himeko_ast.ast_hbcm import AstHbcmTransformer
 from test_ancestor_testcase import TestAncestorTestCase
 
@@ -25,7 +25,8 @@ class TestBasicTransformation(TestAncestorTestCase):
         hyv = hbcm_mapper.convert_tree(root)
         root_el = hyv[-1]
         self.assertEqual(root_el.name, "graph")
-        tensor, n, e = tensor_mapping(root_el)
+        tr = BijectiveCliqueExpansionTransformation()
+        tensor, n, e = tr.encode(root_el)
         self.assertEqual(n, 3)
         self.assertEqual(e, 3)
         x_e1 = np.array([[0, 0, 0], [1, 0, 0], [0, 0, 0]])
@@ -34,6 +35,26 @@ class TestBasicTransformation(TestAncestorTestCase):
         tx = np.array([x_e1,x_e2,x_e3])
         self.assertTrue(np.all(tx == tensor[:, :n, :n]))
 
+    def test_directed3_nodes_1_star_expansion(self):
+        p = "../examples/simple/tensor/directed_3_nodes_1.himeko"
+        root = self.read_node(p)
+        hbcm_mapper = AstHbcmTransformer(NullClock())
+        hyv = hbcm_mapper.convert_tree(root)
+        root_el = hyv[-1]
+        self.assertEqual(root_el.name, "graph")
+        tr = StarExpansionTransformation()
+        tensor, n, e = tr.encode(root_el)
+        self.assertEqual(n, 6)
+        self.assertEqual(e, 3)
+        print(tensor)
+        """
+        x_e1 = np.array([[0, 0, 0], [1, 0, 0], [0, 0, 0]])
+        x_e2 = np.array([[0, 0, 0], [0, 0, 0], [1, 0, 0]])
+        x_e3 = np.array([[0, 0, 0], [0, 0, 1], [0, 0, 0]])
+        tx = np.array([x_e1,x_e2,x_e3])
+        self.assertTrue(np.all(tx == tensor[:, :n, :n]))
+        """
+
     def test_directed3_nodes_2(self):
         p = "../examples/simple/tensor/directed_3_nodes_2.himeko"
         root = self.read_node(p)
@@ -41,7 +62,8 @@ class TestBasicTransformation(TestAncestorTestCase):
         hyv = hbcm_mapper.convert_tree(root)
         root_el = hyv[-1]
         self.assertEqual(root_el.name, "graph")
-        tensor, n, e = tensor_mapping(root_el)
+        tr = BijectiveCliqueExpansionTransformation()
+        tensor, n, e = tr.encode(root_el)
         self.assertEqual(n, 3)
         self.assertEqual(e, 3)
         x_e1 = np.array([[0, 1, 0], [0, 0, 0], [0, 0, 0]])
@@ -57,7 +79,8 @@ class TestBasicTransformation(TestAncestorTestCase):
         hyv = hbcm_mapper.convert_tree(root)
         root_el = hyv[-1]
         self.assertEqual(root_el.name, "graph")
-        tensor, n, e = tensor_mapping(root_el)
+        tr = BijectiveCliqueExpansionTransformation()
+        tensor, n, e = tr.encode(root_el)
         self.assertEqual(n, 4)
         self.assertEqual(e, 5)
         x2_e1 = np.array([[0, 0, 0, 0],
@@ -90,7 +113,8 @@ class TestBasicTransformation(TestAncestorTestCase):
         hyv = hbcm_mapper.convert_tree(root)
         root_el = hyv[-1]
         self.assertEqual(root_el.name, "graph")
-        tensor, n, e = tensor_mapping(root_el)
+        tr = BijectiveCliqueExpansionTransformation()
+        tensor, n, e = tr.encode(root_el)
         self.assertEqual(n, 5)
         self.assertEqual(e, 2)
         x2_e1 = np.array([[0, 0, 0, 0, 0],
@@ -113,7 +137,8 @@ class TestBasicTransformation(TestAncestorTestCase):
         hyv = hbcm_mapper.convert_tree(root)
         root_el = hyv[-1]
         self.assertEqual(root_el.name, "fano")
-        tensor, n, e = tensor_mapping(root_el)
+        tr = BijectiveCliqueExpansionTransformation()
+        tensor, n, e = tr.encode(root_el)
         self.assertEqual(n, 7)
         self.assertEqual(e, 7)
         self.assertTrue(np.all(np.sum(tensor[:, :n, :n], axis=0) - np.eye(7)))
