488c90769f3ae01ef99e8bc4ef96b3314aaf6a1d
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-09-06 20:29:13 +0200

    Making dataflow graph working... better than last time

--- himeko_lang_core/src/processing/parse_description.py
+++ himeko_lang_core/src/processing/parse_description.py
@@ -4,42 +4,86 @@ from lang.himeko_meta_parser import Lark_StandAlone
 from lang.himeko_ast.himeko_ast import transformer
 
 
-class ParseDescriptionEdge(ExecutableHyperEdge):
+class ParserOperation(object):
 
-    def __init__(self, name: str, timestamp: int, serial: int, guid: bytes, suid: bytes, label: str, parent):
-        super().__init__(name, timestamp, serial, guid, suid, label, parent)
+    def __init__(self):
+        self._parser = Lark_StandAlone(transformer=transformer)
+        self._hbcm_mapper = AstHbcmTransformer()
 
-    def execute(self, *args, **kwargs):
-        text = kwargs["text"]
-        # Transformer
-        parser = Lark_StandAlone(transformer=transformer)
-        # Read file
-        root = parser.parse(text)
-        hbcm_mapper = AstHbcmTransformer()
-        if "library_path" in kwargs:
-            library_path = kwargs["library_path"]
-            hyv = hbcm_mapper.convert_tree(root, library_path)
-        else:
-            hyv = hbcm_mapper.convert_tree(root)
-        return hyv
+    @property
+    def parser(self):
+        return self._parser
 
+    def parse(self, text):
+        return self._parser.parse(text)
 
-class ParseDescriptionEdgeFromFile(ExecutableHyperEdge):
-
-    def __init__(self, name: str, timestamp: int, serial: int, guid: bytes, suid: bytes, label: str, parent):
-        super().__init__(name, timestamp, serial, guid, suid, label, parent)
-
-    def execute(self, *args, **kwargs):
-        path = kwargs["path"]
-        # Transformer
-        parser = Lark_StandAlone(transformer=transformer)
-        # Read file
+    def parse_from_file(self, path):
         with open(path) as f:
-            root = parser.parse(f.read())
-        hbcm_mapper = AstHbcmTransformer()
-        if "library_path" in kwargs:
-            library_path = kwargs["library_path"]
-            hyv = hbcm_mapper.convert_tree(root, library_path)
+            return self._parser.parse(f.read())
+
+    def map(self, root, library_path=None):
+        if library_path is not None:
+            return self._hbcm_mapper.convert_tree(root, library_path)
         else:
-            hyv = hbcm_mapper.convert_tree(root)
-        return hyv
+            return self._hbcm_mapper.convert_tree(root)
+
+
+class ParseDescriptionEdge(ExecutableHyperEdge, ParserOperation):
+
+    def __init__(self, name: str, timestamp: int, serial: int, guid: bytes, suid: bytes, label: str, parent):
+        ExecutableHyperEdge.__init__(self, name, timestamp, serial, guid, suid, label, parent)
+        ParserOperation.__init__(self)
+
+    def execute(self, *args, **kwargs):
+        if self.cnt_in_relations < 1:
+            text = kwargs["text"]
+            # Transformer
+            root = self.parser.parse(text)
+            if "library_path" in kwargs:
+                library_path = kwargs["library_path"]
+                hyv = self.map(root, library_path)
+            else:
+                hyv = self.map(root)
+            return hyv
+        else:
+            for v in self.in_relations():
+                library_path = v.target["library_path"].value
+                text = v.target["text"].value
+                root = self.parser.parse(text)
+                res = self.map(root, library_path)
+                for o in self.out_relations():
+                    for r in res:
+                        if r.name not in o.target:
+                            o.target.add_element(r)
+                del res
+
+
+class ParseDescriptionEdgeFromFile(ExecutableHyperEdge, ParserOperation):
+
+    def __init__(self, name: str, timestamp: int, serial: int, guid: bytes, suid: bytes, label: str, parent):
+        ExecutableHyperEdge.__init__(self, name, timestamp, serial, guid, suid, label, parent)
+        ParserOperation.__init__(self)
+
+    def execute(self, *args, **kwargs):
+        if self.cnt_in_relations < 1:
+            path = kwargs["path"]
+            # Transformer
+            # Read file
+            root = self.parse_from_file(path)
+            if "library_path" in kwargs:
+                library_path = kwargs["library_path"]
+                hyv = self.map(root, library_path)
+            else:
+                hyv = self.map(root)
+            return hyv
+        else:
+            for v in self.in_relations():
+                library_path = v.target["library_path"].value
+                path = v.target["path"].value
+                root = self.parse_from_file(path)
+                res = self.map(root, library_path)
+                for o in self.out_relations():
+                    for r in res:
+                        if r.name not in o.target:
+                            o.target.add_element(r)
+                del res
